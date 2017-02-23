d9fc0587890e7baaad77d99f850f144d3338c0f9
Will Kennedy <wildo.waggins@gmail.com>
2017-02-23 08:32:09 -0500

    section_1_3 initial commit

---
+++ section_1/section_1_3/src/main/java/com/github/wkennedy/Section13Application.java
@@ -0,0 +1,12 @@
+package com.github.wkennedy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section13Application.class, args);
+	}
+}
