4704fec98ea4691228fe36b230c115921a92e285
Will Kennedy <wildo.waggins@gmail.com>
2017-03-02 08:00:48 -0500

    Section 2_1 initial commit

---
+++ section_2/section_2_1/src/main/java/com/github/wkennedy/SimpleController.java
@@ -0,0 +1,13 @@
+package com.github.wkennedy;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello!";
+    }
+}
