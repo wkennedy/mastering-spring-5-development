405fb62ca4a2a18d55a68289f732fb962a44f4ae
Will Kennedy <wildo.waggins@gmail.com>
2017-04-28 07:24:26 -0400

    Section_4_2 clean up

--- section_4/section_4_2/src/main/java/com/github/wkennedy/Section42Application.java
+++ section_4/section_4_2/src/main/java/com/github/wkennedy/Section42Application.java
@@ -3,10 +3,8 @@ package com.github.wkennedy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-//import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 
 @SpringBootApplication
-//@Import(SpringDataRestConfiguration.class)
 public class Section42Application {
 
 	public static void main(String[] args) {
